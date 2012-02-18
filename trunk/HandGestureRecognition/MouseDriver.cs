b59638cf9333f8084726a57e6ca1ec7b7cee8d33
neryams <neryams@78b391f7-fddf-7f6c-8e58-6a78b8459f88>
2012-02-18 01:44:33 +0000

    Mouse Driver

---
+++ trunk/HandGestureRecognition/MouseDriver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Drawing;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace HandGestureRecognition
+{
+    class MouseDriver
+    {
+        int pointSetCount;
+        ArrayList pointSets;
+        Contour<PointF> movementContour
+        public MouseDriver()
+        {
+            pointSetCount = 0;
+            pointSets = new ArrayList();
+        }
+        public MouseDriver(Seq<PointF> points, Contour<PointF> movementContour)
+        {
+            pointSetCount = 0;
+            pointSets = new ArrayList();
+            addFrame(points);
+        }
+
+        public void addFrame(Seq<PointF> points, Contour<PointF> movementContour) 
+        {
+            if (pointSetCount < 15)
+                pointSetCount++;
+            else
+                pointSets.RemoveAt(0);
+
+            pointSets.Add(points);
+        }
+    }
+}
