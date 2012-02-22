d76211d3b912a8a5be115f4158496d33869d754c
neryams <neryams@gmail.com>
2012-02-22 14:00:55 +0000

    Added 4 finger minimum initialization and stuff, tried to get the vector evening working right but no dice

--- HandGestureRecognition/Form1.Designer.cs
+++ HandGestureRecognition/Form1.Designer.cs
@@ -30,14 +30,13 @@
         {
             this.imageBoxFrameGrabber = new Emgu.CV.UI.ImageBox();
             this.dataOutput = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBoxFrameGrabber
             // 
             this.imageBoxFrameGrabber.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(0, 2);
+            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(0, 0);
             this.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber";
             this.imageBoxFrameGrabber.Size = new System.Drawing.Size(574, 391);
             this.imageBoxFrameGrabber.TabIndex = 3;
@@ -45,26 +44,17 @@
             // 
             // dataOutput
             // 
-            this.dataOutput.Location = new System.Drawing.Point(583, 12);
+            this.dataOutput.Location = new System.Drawing.Point(592, 12);
             this.dataOutput.Name = "dataOutput";
             this.dataOutput.Size = new System.Drawing.Size(100, 20);
             this.dataOutput.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(580, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.dataOutput.Text = "not watching";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 396);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(704, 391);
             this.Controls.Add(this.dataOutput);
             this.Controls.Add(this.imageBoxFrameGrabber);
             this.Name = "Form1";
@@ -79,7 +69,6 @@
 
         private Emgu.CV.UI.ImageBox imageBoxFrameGrabber;
         private System.Windows.Forms.TextBox dataOutput;
-        private System.Windows.Forms.Label label1;
 
     }
 }
